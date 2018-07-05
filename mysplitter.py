fa6a89fda1032c456dad67d41c6b97e63b7a89f6
Mingrui Jiang <mjray369@gmail.com>
2018-07-06 01:39:19 +0800

    feature: multiprocess

--- mysplitter.py
+++ mysplitter.py
@@ -14,12 +14,13 @@ from Bio.SeqIO.QualityIO import FastqGeneralIterator
 import shutil
 import psutil
 import distance
+import multiprocessing
+
+# cython
 from hamming_cython_solution import hamming_loop
 
 # debugging constants
-FILE_IO_TIME = 0
-CALC_TIME = 0
-WHOLE_LOOP_TIME = 0
+WHOLE_LOOP_TIME = time.time()
 
 process = psutil.Process(os.getpid())
 print('process memory resident set size: {}'.format(process.memory_info().rss))
@@ -43,84 +44,129 @@ infq2 = sys.argv[2]
 
 prefix = sys.argv[3]
 
-'''
-fh1 = gzip.open(infq1,'r')
-fh2 = gzip.open(infq2,'r')
-fq1iter = SeqIO.parse(fh1,'fastq')
-fq2iter = SeqIO.parse(fh2,'fastq')
 
-iter_start_time = time.time()
-for rec1, rec2 in itertools.izip(fq1iter, fq2iter):
-    pass
-print('iter elapsed time: {}'.format(time.time() - iter_start_time))
-fh1.close()
-fh2.close()
-
-WHOLE_LOOP_TIME += time.time() - start_time
-start_time = time.time()
-'''
 fh1 = gzip.open(infq1,'r')
 fh2 = gzip.open(infq2,'r')
 fq1iter = FastqGeneralIterator(fh1)
 fq2iter = FastqGeneralIterator(fh2)
 
-i = 0
-unassigned = 0
-start_time = time.time()
-for (title1, seq1, qual1),(title2, seq2, qual2) in itertools.izip(fq1iter, fq2iter):
-    cal_start_time = time.time()
+def iter_to_list(fq_iter, return_dict, return_key):
+    return_dict[return_key] = list(fq_iter)
 
-    bcd = seq2[0:8]
-    #print(bcd)
-    umi = seq2[8:16]
-    ci = None
-    if bcd in bcds:
-        ci = bcds[bcd]
-    else:
-        for bar in bcds:
-            di = hamming_loop(bcd, bar)
-            if di < 3:
-                bcd = bar
-                ci = bcds[bcd]
-                break
+manager = multiprocessing.Manager()
+return_dict = manager.dict()
+process1 = multiprocessing.Process(target=iter_to_list,
+                                  args=(fq1iter, return_dict, 1))
+process2 = multiprocessing.Process(target=iter_to_list,
+                                   args=(fq2iter, return_dict, 2))
+process1.start()
+process2.start()
+process1.join()
+process2.join()
+fh1.close()
+fh2.close()
 
-    CALC_TIME += time.time()-cal_start_time
 
-    if ci is None:
-        unassigned += 1   
-    else:
-        io_start_time = time.time()
 
-        rec1 = "@%s\n%s\n+\n%s\n" % (title1, seq1, qual1)
-        fo = prefix + '_' + ci + '_' + bcd + '.fastq'
-        fo = os.path.join('output', fo)
-
-        if fo in FILE_DICT:
-            outfile = FILE_DICT[fo]
-            outfile.write(rec1)
-        else:
-            outfile = open(fo, "w+")
-            FILE_DICT[fo] = outfile
-            outfile.write(rec1)
-
-        FILE_IO_TIME += time.time() - io_start_time
-        
-    i = i + 1
-
-    if i%10000==0:
-        elapsed_time = time.time() - start_time
-        print '\n' + str(i) + " has been processed, assigned rate: " + str(1-unassigned/100000.0) + ", consumed time: {}".format(elapsed_time)
-        WHOLE_LOOP_TIME += elapsed_time
+def split(sq1list,sq2list, proc_num):
+    print "process initializing", multiprocessing.current_process()
+    i = 0
+    unassigned = 0
+    FILE_IO_TIME = 0
+    CALC_TIME = 0
+    for (title1, seq1, qual1),(title2, seq2, qual2) in zip(sq1list, sq2list):
         start_time = time.time()
-        print('memory info: {}'.format(process.memory_info()[0]))
-        unassigned = 0
 
-print('WHOLE_LOOP_TIME, CALC_TIME, FILE_IO_TIME, IO_%_TIME: {}, {}, {}, {}'.format(WHOLE_LOOP_TIME, CALC_TIME, FILE_IO_TIME, FILE_IO_TIME/WHOLE_LOOP_TIME))
+        cal_start_time = time.time()
+
+        bcd = seq2[0:8]
+        #print(bcd)
+        umi = seq2[8:16]
+        ci = None
+        if bcd in bcds:
+            ci = bcds[bcd]
+        else:
+            for bar in bcds:
+                di = hamming_loop(bcd, bar)
+                if di < 3:
+                    bcd = bar
+                    ci = bcds[bcd]
+                    break
+
+        CALC_TIME += time.time()-cal_start_time
+
+        if ci is None:
+            unassigned += 1
+        else:
+            io_start_time = time.time()
+
+            rec1 = "@%s\n%s\n+\n%s\n" % (title1, seq1, qual1)
+            fo = str(proc_num) + '_' + ci + '_' + bcd + '.fastq'
+            fo = os.path.join('output', fo)
+
+            if fo in FILE_DICT:
+                outfile = FILE_DICT[fo]
+                outfile.write(rec1)
+            else:
+                outfile = open(fo, "w+")
+                FILE_DICT[fo] = outfile
+                outfile.write(rec1)
+
+            FILE_IO_TIME += time.time() - io_start_time
+
+        i = i + 1
+
+        if i%10000==0:
+            elapsed_time = time.time() - start_time
+            print str(i) + " has been processed, assigned rate: " + str(1-unassigned/100000.0) + ", consumed time: {}".format(elapsed_time)
+            # print('memory info: {}'.format(process.memory_info()[0]))
+            unassigned = 0
+
+
+
+
+
+fq1list = return_dict[1]
+fq2list = return_dict[2]
+
+len1 = len(fq1list)
+fq1list_a = fq1list[:len1*1/4]
+fq1list_b = fq1list[len1*1/4:len1*2/4]
+fq1list_c = fq1list[len1*2/4:len1*3/4]
+fq1list_d = fq1list[len1*3/4:]
+
+len2 = len(fq2list)
+fq2list_a = fq2list[:len2*1/4]
+fq2list_b = fq2list[len2*1/4:len2*2/4]
+fq2list_c = fq2list[len2*2/4:len2*3/4]
+fq2list_d = fq2list[len2*3/4:]
+
+
+
+
+process1 = multiprocessing.Process(target=split,
+                                   args=(fq1list_a, fq2list_a, 1))
+process2 = multiprocessing.Process(target=split,
+                                   args=(fq1list_b, fq2list_b, 2))
+process3 = multiprocessing.Process(target=split,
+                                   args=(fq1list_c, fq2list_c, 3))
+process4 = multiprocessing.Process(target=split,
+                                   args=(fq1list_d, fq2list_d, 4))
+process1.start()
+process2.start()
+process3.start()
+process4.start()
+process1.join()
+process2.join()
+process3.join()
+process4.join()
+
+WHOLE_LOOP_TIME = time.time() - WHOLE_LOOP_TIME
+print('WHOLE_LOOP_TIME: {}'.format(WHOLE_LOOP_TIME))
 
 # closing files
 for k,v in FILE_DICT.iteritems():
     v.close()
 
-fh1.close()
-fh2.close()
+
 
